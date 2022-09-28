98bc1841bc1ce6f9a502e98b26fff85ca05db932
THILAKGATS <thilakrock1799@gmail.com>
2022-09-28 14:58:42 +0530

    LinearSearch1

---
+++ src/main/java/DailyTask/LinearSearch1.java
@@ -0,0 +1,30 @@
+package DailyTask;
+
+import java.util.Scanner;
+
+public class LinearSearch1 {
+	public int lsearch(int[] age1,int key)
+	{
+		for (int i=0;i<age1.length;i++)
+		{
+			if(age1[i]==key)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args)
+	{
+	 
+		int [] age= {1,3,5,6,21,77,22,25,98};
+		Scanner scan=new Scanner(System.in);
+		LinearSearch1 search=new LinearSearch1();
+		System.out.println("Which number you want");
+		int key=scan.nextInt();
+		System.out.println(key+" your values is founded by"+search.lsearch(age,key));
+	}
+
+
+}
