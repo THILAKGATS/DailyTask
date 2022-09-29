2fadae12b887b1d26ede44e151100efd3dcb4c97
THILAKGATS <thilakrock1799@gmail.com>
2022-09-29 15:21:48 +0530

    Oddoreven updated

--- src/main/java/DailyTask/OddEven.java
+++ src/main/java/DailyTask/OddEven.java
@@ -4,15 +4,20 @@ public class OddEven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] arr = {1,2,3,4,5,6,7,8,9,10};
-		for(int i=0;i<10;i+=2)
-		{
-			System.out.print(arr[i]);
-		}
-		for(int i=1;i<10;i+=2)
-		{
-			System.out.print(arr[i]);
-		}
+		int a=10;
+		int b=10;
+		int [] arr = {0,1,2,3,4,5,6,7,8,9};
+		for(int i=0;i<arr.length;i++)	
+			if(arr[i]%2!=0)//Odd Numbers
+			{
+				System.out.print(arr[i]);
+				
+			}
+		for(int j=0;j<arr.length;j++)
+			if(arr[j]%2==0)//Even NUmbers
+			{
+				System.out.print(arr[j]);
+			}
 	}
 
 }	
